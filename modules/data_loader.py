74041adeb3745415de8e691081c9607ab5692834
Albert Marques <hygor.marques@protonmail.com>
2025-11-19 14:24:40 -0300

    get functions defined

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -33,3 +33,36 @@ def load_data():
     
     print("Dados carregados com sucesso")
     return df
+
+def get_esf_list(df):
+    """
+    Retorna uma lista ordenada e única de todos os nomes de ESF.
+    """
+    if df is not None:
+        return sorted(df[COLUNA_ESF].unique())
+    return []
+
+def get_chart_columns(df):
+    """
+    Retorna a lista de colunas que podem ser usadas para gerar gráficos categóricos.
+    Vamos incluir todas as colunas que não são as colunas de identificação e tempo.
+    """
+    colunas_para_ignorar = [
+        'Carimbo de data/hora',
+        COLUNA_ESF,
+        'Nome do entrevistador',
+        'Nome do Paciente',
+        'Data de nascimento do paciente',
+        'Sexo da pessoa atendida',
+        'Idade da pessoa atendida   ',
+        '  Grau de Instrução da pessoa atendida  '
+    ]
+
+    charts_cols = [col for col in df.columns if col not in colunas_para_ignorar]
+    return charts_cols
+
+SUGGESTED_CHART_COLUMNS = [
+    'Quanto tempo você levou para conseguir essa consulta?   ',
+    'Como você avalia a forma de marcação das consultas?   ',
+    'Como você avalia a forma de marcação de exames laboratoriais?   '
+]
